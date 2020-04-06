393322dd8e55fca46a351f39fc54f3e59ffd1b97
Hydra <umeraqeel11@gmail.com>
2020-04-07 01:57:40 +0500

    find elements

--- src/test/java/base/BaseTests.java
+++ src/test/java/base/BaseTests.java
@@ -1,8 +1,12 @@
 package base;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.Dimension;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.util.List;
 
 public class BaseTests {
 
@@ -11,10 +15,27 @@ public class BaseTests {
     public void setUp(){
         System.setProperty("webdriver.chrome.driver", "resources/chromedriver.exe");
         driver = new ChromeDriver();
+        //Web page URL
         driver.get("https://the-internet.herokuapp.com/");
 
+        //Find and Print menu elements.
+        //driver.findElement(By.linkText("Shifting Content")).click();
+        //driver.findElement(By.linkText(("Example 1: Menu Element"))).click();
+        //List<WebElement> menuItems = driver.findElements(By.tagName("li"));
+        //System.out.println("Number of menu items: " + menuItems.size());
+
+
+        //Find element by List
+        //List<WebElement> link = driver.findElements(By.tagName("a"));
+        //System.out.println(link.size());
+
+        //Find Elements BY Singular
+        //WebElement inputsLink = driver.findElement(By.linkText("Shifting Content"));
+        //inputsLink.click();
+
         //WebDriver command to modify the window size maximize, fullscreen, setSize(new Dimension()).
-        driver.manage().window().setSize(new Dimension(375,812));
+        //driver.manage().window().setSize(new Dimension(375,812));
+
         System.out.println(driver.getTitle());
         driver.quit();
     }
